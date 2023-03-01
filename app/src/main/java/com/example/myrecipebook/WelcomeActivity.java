c0430312f0a370c0d249280b27e48f7668d4dd05
shani bider <shanibider@gmail.com>
2023-03-01 17:34:38 +0200

    Add WelcomeActivity and RegisterActivity

--- app/src/main/java/com/example/myrecipebook/WelcomeActivity.java
+++ app/src/main/java/com/example/myrecipebook/WelcomeActivity.java
@@ -2,13 +2,25 @@ package com.example.myrecipebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this,RegisterActivity.class));
     }
 }
