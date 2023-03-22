0082ece9defc15117906dee1c1e9efe1d01727fd
shani bider <shanibider@gmail.com>
2023-03-22 13:02:38 +0200

    adding files

---
+++ app/src/main/java/com/example/myrecipebook/activities/MyRecipeUpdateActivity.java
@@ -0,0 +1,168 @@
+package com.example.myrecipebook.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.myrecipebook.DataClass;
+import com.example.myrecipebook.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
+public class MyRecipeUpdateActivity extends AppCompatActivity {
+
+    ImageView updateImage;
+    Button updateButton;
+
+    EditText updateTitle,updateIngr, updateInst, updateTime;
+    String title, desc, lang;
+
+    String imageUrl;
+    String key, oldImageURL;
+    Uri uri;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_my_recipe_update);
+
+        updateTitle = findViewById(R.id.updateTitle);
+        updateButton = findViewById(R.id.updateButton);
+        updateIngr = findViewById(R.id.updateIngr);
+        updateInst = findViewById(R.id.updateInst);
+        updateImage = findViewById(R.id.updateImage);
+        updateTime = findViewById(R.id.updateTime);
+
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            uri = data.getData();
+                            updateImage.setImageURI(uri);
+                        } else {
+                            Toast.makeText(MyRecipeUpdateActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null){
+            Glide.with(MyRecipeUpdateActivity.this).load(bundle.getString("Image")).into(updateImage);
+            updateTitle.setText(bundle.getString("Title"));
+            updateIngr.setText(bundle.getString("Description"));
+            updateInst.setText(bundle.getString("Language"));
+            key = bundle.getString("Key");
+            oldImageURL = bundle.getString("Image");
+        }
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Android Tutorials").child(key);
+
+        updateImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
+
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                Intent intent = new Intent(MyRecipeUpdateActivity.this, MyRecipesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    public void saveData(){
+        storageReference = FirebaseStorage.getInstance().getReference().child("Android Images").child(uri.getLastPathSegment());
+        AlertDialog.Builder builder = new AlertDialog.Builder(MyRecipeUpdateActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete());
+                Uri urlImage = uriTask.getResult();
+                imageUrl = urlImage.toString();
+                updateData();
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+
+    public void updateData(){
+        //change this
+        title = updateTitle.getText().toString().trim();
+        desc = updateIngr.getText().toString().trim();
+        lang = updateInst.getText().toString();
+        DataClass dataClass = new DataClass(title, desc, lang, imageUrl);
+
+
+
+
+        databaseReference.setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    StorageReference reference = FirebaseStorage.getInstance().getReferenceFromUrl(oldImageURL);
+                    reference.delete();
+                    Toast.makeText(MyRecipeUpdateActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MyRecipeUpdateActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
