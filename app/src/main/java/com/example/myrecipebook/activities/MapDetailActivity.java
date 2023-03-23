7a58eba3ad6311f0fbdc047f446a29fb316aed67
shani bider <shanibider@gmail.com>
2023-03-23 12:31:39 +0200

    adding comments

--- app/src/main/java/com/example/myrecipebook/activities/MapDetailActivity.java
+++ app/src/main/java/com/example/myrecipebook/activities/MapDetailActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+// retrieves the details of a location from a Firebase Realtime Database and displays them
 public class MapDetailActivity extends AppCompatActivity {
 
     TextView storeTitle;
@@ -29,21 +30,28 @@ public class MapDetailActivity extends AppCompatActivity {
         address = findViewById(R.id.AdrressName);
         hours = findViewById(R.id.StoreHours);
 
+        //retrieves any extras that were passed to the activity
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
+            //and updates the views with the markerTitle value
             String markerTitle = extras.getString("markerTitle");
             storeTitle.setText(markerTitle);
             updateTexts(markerTitle);
         }
     }
 
+    //retrieves data from a Firebase Realtime Database using a DatabaseReference
     private void updateTexts(String markerTitle)
     {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference reference = database.getReference("locations");
+
+        //adds valueEventListener to the reference, which listens for changes to the data at the location referenced by markerTitle
         reference.child(markerTitle).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
+            //the onDataChange() method is called with a DataSnapshot containing the updated data
             public void onDataChange(DataSnapshot dataSnapshot) {
+                //The method then extracts the address and hours from the LocationMarker object and updates the corresponding views
                 LocationMarker locationMarker = dataSnapshot.getValue(LocationMarker.class);
                 if(locationMarker != null)
                 {
@@ -59,3 +67,22 @@ public class MapDetailActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
+/*
+Bundle-
+used to store and transfer data between activities, fragments, and services within an app. It's a way to pass data from one component of the app to another without having to rely on global variables.
+A bundle can be used to store any type of data that can be serialized and deserialized.
+To create a bundle, you can use the putExtra() method on an Intent object, which is typically used to start a new activity or service.
+In the receiving activity, you can retrieve the data from the bundle using the getXXX().
+
+For example:
+// Get the Bundle object from the Intent
+Bundle bundle = getIntent().getExtras();
+
+// Get the data from the Bundle
+String value1 = bundle.getString("key1");
+int value2 = bundle.getInt("key2");
+
+*/
